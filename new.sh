5bec201a8853d859ee39741296a81838521b4ef8
jiap <jiap@hotmail.com>
2020-04-03 11:54:40 +0800

    feat: add non-uniform-problem-2 & feat: revise shell script

--- new.sh
+++ new.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-
 file_name="$1"
+timestamp=$(date "+%Y-%m-%d %H:%M:%S")
+file_name2=${file_name:11}
 
-echo '---
+echo "---
 title: 
-url: ${file_name:11}
-date: $(date "+%Y-%m-%d %H:%M:%S")
+url: $file_name2
+date: $timestamp
 tags:
+- 
 categories: 
 description: 
 ---
-<img src="/assets/images/[article dir]/[pic name]" style="zoom:40%; margin: 0 auto; display: block;" />' > "_posts/"$1".md"
+
+<img src='/assets/images/$file_name/[pic name]' style='zoom:40%; margin: 0 auto; display: block;'/>" > "_posts/"$file_name".md"
 
 mkdir "assets/images/$file_name"
